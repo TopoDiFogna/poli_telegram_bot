a9a318de78e09f0e3b5a241790e74d869a3c384b
Lorenzo <Lorenzo@DESKTOP-3V1AU2R>
2016-04-04 12:53:08 +0200

    Added switch for managing message type

--- bot.php
+++ bot.php
@@ -5,14 +5,37 @@ function processMessage($message) {
 	
 	$chat_id = $message ['chat'] ['id'];//chat to send the message to
 	
-	$text_message = $message['text'];//what has been sent to the bot
-	
+	switch ($message){ 
+		case array_key_exists('text', $message): 
+			$text_message = $message['text'];
+			processTextMessage($text_message);
+			break;
+		case array_key_exists('audio', $message):
+			break;
+		case array_key_exists('document', $message):
+			break;
+		case array_key_exists('sticker', $message):
+			break;
+		case array_key_exists('video', $message):
+			break;
+		case array_key_exists('voice', $message):
+			break;
+		case array_key_exists('contact', $message):
+			break;
+		case array_key_exists('location', $message):
+			break;
+		default:
+			break;
+	}
+}
+
+function processTextMessage($text_message){
 	if (isset ( $text_message )) {
-		
+	
 		$text = $message ['text']; //incoming text message
-		
+	
 		if (strpos ( $text, "/start" ) === 0) {
-			
+				
 			$file=fopen("start.txt", "r");
 			$response=fread($file, filesize("start.txt"));
 			fclose($file);
@@ -20,30 +43,30 @@ function processMessage($message) {
 					'chat_id' => $chat_id,
 					'text' => $response,
 					'parse_mode' => 'Markdown',
-					) 
-			);
+			)
+					);
 		} else if ($text === "Hello" || $text === "Hi") {
-			
+				
 			apiRequestJson ( "sendMessage", array (
 					'chat_id' => $chat_id,
-					"text" => 'Nice to meet you' 
+					"text" => 'Nice to meet you'
 			) );
 		} else if (strpos ( $text, "/stop" ) === 0) {
-			
+				
 			// stop now
 		} else {
-			
+				
 			apiRequestJson( "sendMessage", array (
 					'chat_id' => $chat_id,
 					"reply_to_message_id" => $message_id,
-					"text" => 'Cool' 
+					"text" => 'Cool'
 			) );
 		}
 	} else {
-		
+	
 		apiRequestJson ( "sendMessage", array (
 				'chat_id' => $chat_id,
-				"text" => 'I understand only text messages' 
+				"text" => 'I understand only text messages'
 		) );
 	}
 }
