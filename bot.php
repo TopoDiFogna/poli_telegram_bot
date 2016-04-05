db047dcb19d412fd80cb6bf59d5ca1726139930f
Paolo Paterna <paolo.paterna@gmail.com>
2016-04-05 16:14:40 +0200

    modified switch statement when parsing message

--- bot.php
+++ bot.php
@@ -29,81 +29,58 @@ function processMessage($message) {
 	}
 }
 function processTextMessage($text, $chat_id, $message_id) {
-	if (isset ( $text )) {
-		switch ($text){
-			case (strpos($text, "/start")==0):
-				startFunction($chat_id, $message_id);
-				break;
-			case ($text === "Hello" || $text === "Hi"):
-				apiRequestJson ( "sendMessage", array (
-						'chat_id' => $chat_id,
-						"text" => 'Nice to meet you'
-				) );
-				break;
-			case (strpos ( $text, "/stop" ) === 0):
-				break;
-			case (strpos ( $text, "/occupation" ) === 0):
-				occupationOfTheDay($chat_id);
-				break;
-			default:
-				piRequestJson ( "sendMessage", array (
-						'chat_id' => $chat_id,
-						"reply_to_message_id" => $message_id,
-						"text" => 'Cool'
-				) );
-				break;
-		}
-	} else {
-		apiRequestJson ( "sendMessage", array (
-				'chat_id' => $chat_id,
-				'reply_to_message_id' => $message_id,
-				'text' => 'You Send me an empty text Message, fool !!' 
-		) );
+	switch ($text) {
+		case strpos ( $text, "/start" ) == 0 :
+			startFunction ( $chat_id, $message_id );
+			break;
+		case strpos ( $text, "/stop" ) === 0 :
+			break;
+		case strpos ( $text, "/occupation" ) === 0 :
+			occupationOfTheDay ( $chat_id );
+			break;
+		default :
+			sendMessage ( $chat_id, "Cool", array (
+					"reply_to_message_id" => $message_id 
+			) );
+			break;
 	}
 }
 function startFunction($chat_id, $message_id) {
 	$file = fopen ( "start.txt", "r" );
 	$response = fread ( $file, filesize ( "start.txt" ) );
 	fclose ( $file );
-	sendMessage($chat_id, $response, array (
+	sendMessage ( $chat_id, $response, array (
 			'parse_mode' => 'Markdown' 
 	) );
-/* 	apiRequestJson ( "sendMessage", array (
-			'chat_id' => $chat_id,
-			'text' => $response,
-			'parse_mode' => 'Markdown' 
-	) ); */
 }
 function occupationOfTheDay($chat_id) {
-	$day=date('j');
-	$month=date('n');
-	$year=date('Y');
-	$time=data('H:i:s',time());
-	$lastDate=getLastDate();
-	$splitted=explode('-', $lastDate);
+	$day = date ( 'j' );
+	$month = date ( 'n' );
+	$year = date ( 'Y' );
+	$time = data ( 'H:i:s', time () );
+	$lastDate = getLastDate ();
+	$splitted = explode ( '-', $lastDate );
 	
-	$url='https://www7.ceda.polimi.it/spazi/spazi/controller/OccupazioniGiornoEsatto.do?csic=MIA&categoria=D&tipologia=tutte&giorno_day='.$day.'&giorno_month='.$month.'&giorno_year='.$year.'&jaf_giorno_date_format=dd%2FMM%2Fyyyy&evn_visualizza=Visualizza+occupazioni';
-	$result=getHTMLCurlResponse($url);
+	$url = 'https://www7.ceda.polimi.it/spazi/spazi/controller/OccupazioniGiornoEsatto.do?csic=MIA&categoria=D&tipologia=tutte&giorno_day=' . $day . '&giorno_month=' . $month . '&giorno_year=' . $year . '&jaf_giorno_date_format=dd%2FMM%2Fyyyy&evn_visualizza=Visualizza+occupazioni';
+	$result = getHTMLCurlResponse ( $url );
 	
-	$domOfHTML=getDOMFromHTMLIDWithCSS($result, 'tableContainer', "spazi/table-MOZ.css");
+	$domOfHTML = getDOMFromHTMLIDWithCSS ( $result, 'tableContainer', "spazi/table-MOZ.css" );
 	
-	$myfile=fopen("occupation.html","w");
-	fwrite($myfile,$domOfHTML->saveHTML());
-	fclose($myfile);
-	$fileNamePath=realpath('occupation.html');
+	$myfile = fopen ( "occupation.html", "w" );
+	fwrite ( $myfile, $domOfHTML->saveHTML () );
+	fclose ( $myfile );
+	$fileNamePath = realpath ( 'occupation.html' );
 	
-	$cmdLine='xvfb-run --server-args="-screen 0, 1024x768x24" /var/www/telegrambot/webkit2png.py -o /var/www/telegrambot/occupation.png /var/www/telegrambot/occupation.html';
-	shell_exec($cmdLine);
+	$cmdLine = 'xvfb-run --server-args="-screen 0, 1024x768x24" /var/www/telegrambot/webkit2png.py -o /var/www/telegrambot/occupation.png /var/www/telegrambot/occupation.html';
+	shell_exec ( $cmdLine );
 	
-	sendNewFile( "sendDocument", array (
+	sendNewFile ( "sendDocument", array (
 			'chat_id' => $chat_id,
-			'document' => new CURLFile($fileNamePath),
+			'document' => new CURLFile ( $fileNamePath ) 
 	) );
-	
 }
-
-function getHTMLCurlResponse($url){
-	$options = array(
+function getHTMLCurlResponse($url) {
+	$options = array (
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_HEADER => false,
 			CURLOPT_FOLLOWLOCATION => true,
@@ -112,29 +89,28 @@ function getHTMLCurlResponse($url){
 			CURLOPT_AUTOREFERER => true,
 			CURLOPT_ENCODING => '',
 			CURLOPT_CONNECTTIMEOUT => 120,
-			CURLOPT_TIMEOUT => 120,
+			CURLOPT_TIMEOUT => 120 
 	);
-	$ch=curl_init($url);
-	curl_setopt_array($ch,$options);
-	$content=curl_exec($ch);
-	curl_close($ch);
+	$ch = curl_init ( $url );
+	curl_setopt_array ( $ch, $options );
+	$content = curl_exec ( $ch );
+	curl_close ( $ch );
 	return $content;
 }
-
-function getDOMFromHTMLIDWithCSS($page,$idToSelect,$cssFilePath){
-	$dom=new DOMDocument();
-	$internalErrors = libxml_use_internal_errors(true);
-	$dom->loadHTML($page);
-	$my=$dom->getElementById($idToSelect);
-	$newdom=new DOMDocument();
-	$cloned=$my->cloneNode(TRUE);
-	$styleFile=fopen($cssFilePath,"r");
-	$style=fread($styleFile, filesize($cssFilePath));
-	fclose($styleFile);
-	$element=$newdom->createElement('style',$style);
-	$newdom->appendChild($element);
-	$newdom->appendChild($newdom->importNode($cloned, TRUE));
-	libxml_use_internal_errors($internalErrors);
+function getDOMFromHTMLIDWithCSS($page, $idToSelect, $cssFilePath) {
+	$dom = new DOMDocument ();
+	$internalErrors = libxml_use_internal_errors ( true );
+	$dom->loadHTML ( $page );
+	$my = $dom->getElementById ( $idToSelect );
+	$newdom = new DOMDocument ();
+	$cloned = $my->cloneNode ( TRUE );
+	$styleFile = fopen ( $cssFilePath, "r" );
+	$style = fread ( $styleFile, filesize ( $cssFilePath ) );
+	fclose ( $styleFile );
+	$element = $newdom->createElement ( 'style', $style );
+	$newdom->appendChild ( $element );
+	$newdom->appendChild ( $newdom->importNode ( $cloned, TRUE ) );
+	libxml_use_internal_errors ( $internalErrors );
 	return $newdom;
 }
 ?>
