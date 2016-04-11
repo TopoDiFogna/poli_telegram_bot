f4966b9050efb1d85f08eb1a49c3bf7ddac503c2
Paolo Paterna <paolo.paterna@gmail.com>
2016-04-11 18:39:40 +0200

    Documented index.php

--- index.php
+++ index.php
@@ -3,15 +3,17 @@ include_once 'network.php';
 include_once 'bot.php';
 include_once 'xmlUtils.php';
 
-//read data from the request
+//reads the data from the request
 $content = file_get_contents("php://input");
-$result = json_decode($content, true); //decode the json request
+
+//decode the json request
+$result = json_decode($content, true);
 
 //if the request is empty just exit
 if (!count($result)) {
 	exit;
 }
-else {//therwise process the message
+else {//otherwise process the message
 	processMessage($result["message"]);
 }
 ?>
