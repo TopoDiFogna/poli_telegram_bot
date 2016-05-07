6002a7a7afe6deedfd4a87fcdfb5ae685c065b49
LorenzoZoia <lollozoia@hotmail.it>
2016-05-07 10:54:14 +0200

    Sono un babbione

--- objectFree.php
+++ objectFree.php
@@ -82,9 +82,9 @@ class freeObj {
 		}
 	}
 	public function executeCommandFree() {
-		$deltahour = intval($this->endTimeH) - intval($this->starTimeH);
+		$deltahour = intval($this->endTimeH) - intval($this->startTimeH);
 		error_log($this->endTimeH." ".$this->startTimeH);
-		error_log(intval($this->endTimeH)." ".intval($this->starTimeH));
+		error_log(intval($this->endTimeH)." ".intval($this->startTimeH));
 		error_log("--------------------------".$deltahour);
 		return false;
 		if ($deltahour > 0) {
